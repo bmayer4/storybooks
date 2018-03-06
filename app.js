7898512792646693a787c673cc31b52224c0be3d
bmayer4 <bmayer55@yahoo.com>
2018-03-05 20:23:11 -0500

    init commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
+    console.log('get route working');
     res.send('start me up!');
 });
 
