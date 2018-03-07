9560bb5876a52732cba5418c98480dcc46187b1b
bmayer4 <bmayer55@yahoo.com>
2018-03-06 23:38:59 -0500

    auth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //commit this so app knows what to do in dev or prod
-
+console.log('env is: ', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');  //set up heroku env variables
 } else {
