b63b1cadad5525a0de5a1a751f3ff95e021333ff
bmayer4 <bmayer55@yahoo.com>
2018-03-06 23:13:49 -0500

    small auth change

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //commit this so app knows what to do in dev or prod
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
+    module.exports = require('./dev');
 } else {
-    module.exports = require('./prod');  //need to change this
+    module.exports = require('./dev');  //need to change this
 }
